03c5b21fbf54afa7289a6f0b91b824c31299bca4
kalpanabharath <stonepanakeerthu@gmail.com>
2025-02-07 22:02:37 +0530

    product

---
+++ scripts/home-page.js
@@ -0,0 +1,55 @@
+
+
+AOS.init();
+// document.addEventListener("DOMContentLoaded", function () {
+//     const dropdowns = document.querySelectorAll(".nav-item.dropdown");
+
+//     dropdowns.forEach(dropdown => {
+//       dropdown.addEventListener("mouseenter", function () {
+//         this.querySelector(".dropdown-menu").classList.add("show");
+//       });
+
+//       dropdown.addEventListener("mouseleave", function () {
+//         this.querySelector(".dropdown-menu").classList.remove("show");
+//       });
+//     });
+//   });
+
+
+// swiper start
+var swiper = new Swiper(".mySwiper", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    coverflowEffect: {
+      rotate: 50,
+      stretch: 0,
+      depth: 100,
+      modifier: 1,
+      slideShadows: true,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+    },
+  });
+// swiper end
+
+document.querySelectorAll(".counter").forEach((item) => {
+  const targetValue = Number(item.dataset.value);
+  const increment = Number(item.dataset.plus);
+  const suffix = item.textContent.trim();
+  let currentCount = 0;
+
+  const updateCounter = () => {
+      if (currentCount >= targetValue) {
+          item.textContent = `${targetValue}${suffix}`;
+          clearInterval(interval);
+          return;
+      }
+      item.textContent = `${currentCount}${suffix}`;
+      currentCount += increment;
+  };
+
+  const interval = setInterval(updateCounter, 100);
+});
