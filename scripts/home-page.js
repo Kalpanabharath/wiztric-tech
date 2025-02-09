d65da097fbf09f01b263477667195e822a72aa8f
kalpana Bharath <stonepanakeerthu@gmail.com>
2025-02-09 20:19:01 +0530

    footer

--- scripts/home-page.js
+++ scripts/home-page.js
@@ -1,5 +1,3 @@
-
-
 AOS.init();
 // document.addEventListener("DOMContentLoaded", function () {
 //     const dropdowns = document.querySelectorAll(".nav-item.dropdown");
@@ -15,24 +13,23 @@ AOS.init();
 //     });
 //   });
 
-
 // swiper start
 var swiper = new Swiper(".mySwiper", {
-    effect: "coverflow",
-    grabCursor: true,
-    centeredSlides: true,
-    slidesPerView: "auto",
-    coverflowEffect: {
-      rotate: 50,
-      stretch: 0,
-      depth: 100,
-      modifier: 1,
-      slideShadows: true,
-    },
-    pagination: {
-      el: ".swiper-pagination",
-    },
-  });
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  coverflowEffect: {
+    rotate: 50,
+    stretch: 0,
+    depth: 100,
+    modifier: 1,
+    slideShadows: true,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
 // swiper end
 
 document.querySelectorAll(".counter").forEach((item) => {
@@ -42,14 +39,40 @@ document.querySelectorAll(".counter").forEach((item) => {
   let currentCount = 0;
 
   const updateCounter = () => {
-      if (currentCount >= targetValue) {
-          item.textContent = `${targetValue}${suffix}`;
-          clearInterval(interval);
-          return;
-      }
-      item.textContent = `${currentCount}${suffix}`;
-      currentCount += increment;
+    if (currentCount >= targetValue) {
+      item.textContent = `${targetValue}${suffix}`;
+      clearInterval(interval);
+      return;
+    }
+    item.textContent = `${currentCount}${suffix}`;
+    currentCount += increment;
   };
 
   const interval = setInterval(updateCounter, 100);
+});
+
+// slick
+
+$(document).ready(function () {
+  $(".leadership-slider").slick({
+    dots: false,
+    arrows: true,
+    infinite: false,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 576,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
+  });
 });
